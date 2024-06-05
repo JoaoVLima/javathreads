b9c3efdf9dd20428a0ef864d708bd9717e5e8be7
João Lima <celu.joaolima@gmail.com>
2024-06-05 16:48:25 -0300

    implementacao 4

--- src/Implementacao4.java
+++ src/Implementacao4.java
@@ -4,42 +4,72 @@
     2. Inicializa o vetor alocado anteriormente com valores aleatórios
     3. Após a inicialização exibe uma mensagem ‘Encerrou inicalizacao’
     4. Computa e exibe a quantidade de valores no vetor que são maiores que 0.25 e menores que 0.75
+
+    Calcule o tempo de execução variando a quantidade de threads de 1 a 4
+
+    Cuidado com problema de memoria.
 */
 
+import java.util.Random;
+
 class MinhaThread4 extends Thread {
+    private volatile double[] array;
+    private int array_size;
+    public int acc;
 
-    private String nome;
-
-    public MinhaThread4(String nome) {
-        this.nome = nome;
+    public MinhaThread4(int array_size) {
+        this.array_size = array_size;
     }
 
     public void run() {
-        System.out.println("Bem Vindo!");
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        this.array = new double[this.array_size];
+
+        Random rand = new Random();
+        for (int i = 0; i < this.array_size; i++) {
+            this.array[i] = rand.nextDouble();
         }
-        System.out.println("Adeus!");
+        System.out.println("Encerrou inicalizacao");
+
+        this.acc = 0;
+        for (int i = 0; i < this.array_size; i++) {
+            if (this.array[i] > 0.25 & this.array[i] < 0.75){
+                this.acc++;
+            }
+        }
+
     }
 }
 
 public class Implementacao4 {
 
+    public static void CriarThreads(int numThreads, int array_size) throws InterruptedException {
+        MinhaThread4[] threads = new MinhaThread4[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new MinhaThread4(array_size/numThreads);
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        // Soma os valores
+        int acc = 0;
+        for (MinhaThread4 thread : threads) {
+            acc+=thread.acc;
+        }
+        System.out.println("acc: " + acc);
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        long inicio = System.currentTimeMillis();
 
+        CriarThreads(4, 200_000_000);
 
-        Thread t1 = new MinhaThread4("t1");
-        Thread t2 = new MinhaThread4("t2");
-
-        t1.start();
-        t1.join();
-        t2.start();
-        t2.join();
-
-        System.out.println("Fim do programa!");
-
+        long fim = System.currentTimeMillis();
+        long duracao = (fim - inicio);
+        System.out.println(duracao + "ms");
     }
 
 }
